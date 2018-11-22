a269aec565b3b2f650dd615bacb65ee76e51f3be
Andres Correa <jcorremo@everis.com>
2018-11-22 13:51:29 -0500

    script

--- build_and_run.sh
+++ build_and_run.sh
@@ -3,4 +3,4 @@ echo $(pwd)
 echo $(ls -ltr)
 JMX_OPTIONS="-Dcom.sun.management.jmxremote=true -Dcom.sun.management.jmxremote.port=7778 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Djava.rmi.server.hostname=paradigma.cursovertx "
 #mvn clean package -DskipTests 
-java $JMX_OPTIONS -Dvertx.logger-delegate-factory-class-name=io.vertx.core.logging.SLF4JLogDelegateFactory -jar target/clientes-1.0.0-SNAPSHOT-fat.jar -conf src/config/config.json  
+java $JMX_OPTIONS -Dvertx.logger-delegate-factory-class-name=io.vertx.core.logging.SLF4JLogDelegateFactory -jar clientes-1.0.0-SNAPSHOT-fat.jar -conf src/config/config.json  
